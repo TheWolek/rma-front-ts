9447cf22c20417084b5a9942557cd02c7557302c
TheWolek <pawelm916@wp.pl>
2023-12-16 20:30:07 +0100

    add button to ticket form on ticket list

--- src/components/RMA/rmaList/RmaListActions.vue
+++ src/components/RMA/rmaList/RmaListActions.vue
@@ -1,8 +1,9 @@
 <script setup lang="ts">
+import router from "@/router";
+import { storeToRefs } from "pinia";
 import { useRmaStore } from "@/stores/RMA";
 import ActionButton from "@/components/parts/buttons/ActionButton.vue";
 import ActionButtonRefresh from "@/components/parts/buttons/ActionButtonRefresh.vue";
-import { storeToRefs } from "pinia";
 
 const store = useRmaStore();
 const { filtersModalActive, loadingRmaList } = storeToRefs(store);
@@ -11,12 +12,19 @@ const toggleFiltersModal = () => {
   filtersModalActive.value = !filtersModalActive.value;
 };
 
+const redirectToAddPage = () => {
+  router.push({
+    name: "rmaAdd",
+  });
+};
+
 const onRefresh = () => {
   store.fetchTicketListByFilters();
 };
 </script>
 <template>
   <div class="actions">
+    <ActionButton :event="redirectToAddPage" display="Nowy" :icon="`add.svg`" />
     <ActionButton
       :event="toggleFiltersModal"
       display="Filtry"
